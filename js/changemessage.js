4a9fe04a09deaf744064916e9a00d93425d3fd5b
Minor Andrés Umaña Murillo <andresmrllo@gmail.com>
2017-03-09 17:14:31 -0600

    Front page ready. Function message ready. Contact form ready
    Remove the message quotes.
    Fuction message edited.
    Added the host email to contactme.php. noreply@theplanid.com,
    info@theplanid.com

--- js/changemessage.js
+++ js/changemessage.js
@@ -1,19 +1,27 @@
-//Obtiene el numero del url http://theplanid.com?mms=#
-var numbermms = location.search.split('mms=')[1]
+(function($){
+  var messagetoshow;
+  //Obtiene el numero del url http://theplanid.com?mms=#
+  var numbermms = location.search.split('mms=')[1]
 
-//Obtiene la clase del source html
-var $message = document.querySelector('.message');
+  //Obtiene la clase del source html
+  var $message = document.querySelector('.message');
 
-//Se enlistan los mensajes
-var messages = [
-"Optimización a través de ejecución efectiva.",
-"Productividad a través de integración de procesos.",
-"Ejecución oportuna de procesos.",
-"Productividad a través de ejecución oportuna de procesos."
-];
+  //Se enlistan los mensajes
+  var messages = [
+  "Optimización a través de ejecución efectiva.",
+  "Productividad a través de integración de procesos.",
+  "Ejecución oportuna de procesos.",
+  "Productividad a través de ejecución oportuna de procesos."
+  ];
+  //Se obtiene el mensaje por medio del numero
+  if(numbermms == null)
+  {
+    messagetoshow = messages[0];
+  }
+  else {
+    messagetoshow = messages[numbermms];
+  }
 
-//Se obtiene el mensaje por medio del numero
-var messagetoshow = messages[numbermms];
-
-//Se agrega el mensaje al source html
-$message.innerHTML = messagetoshow;
+  //Se agrega el mensaje al source html
+  $message.innerHTML = messagetoshow;
+})(jQuery);
