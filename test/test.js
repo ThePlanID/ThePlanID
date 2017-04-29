bcdc4133d75c28c0081ea243ffbc98d29add0cdc
Zyoruk <ce.zyoruk@gmail.com>
2017-04-29 17:49:58 -0600

    testing firebase

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var config = {
+  apiKey: "AIzaSyCieYqszAeC4jAH8oqAtxLM3cN174nlZAc",
+  authDomain: "theplanid-895f3.firebaseapp.com",
+  databaseURL: "https://theplanid-895f3.firebaseio.com",
+  projectId: "theplanid-895f3",
+  storageBucket: "theplanid-895f3.appspot.com",
+  messagingSenderId: "405991042272"
+};
+firebase.initializeApp(config);
+
+
+// Get a reference to the database service
+var database = firebase.database();
