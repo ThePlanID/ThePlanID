c993ded1de4cafc30498c44568da9834d35c183a
Minor Andrés Umaña Murillo <andresmrllo@gmail.com>
2017-12-05 19:02:36 -0600

    init on local

--- app/functions/myfuncts/create.js
+++ app/functions/myfuncts/create.js
@@ -1,19 +1,24 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
-try {admin.initializeApp(functions.config().firebase);} catch(e) {} // You do that because the admin SDK can only be initialized once.
 
 exports = module.exports =
-    functions.https.onRequest((request, response) => 
-    {
-        const email, password;
-        request.body.hasOwnProperty("email") ? email = request.body["email"] : response.send("303", "invalid format");
-        request.body.hasOwnProperty("password") ? password = request.body["password"] : response.send("303", "invalid format");
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ...
-            response.send ("303", "Failed user");
-          });
-    }
+    functions.https.onRequest(
+        (request, response) => 
+        {
+            var email, password;
+            request.body.hasOwnProperty("email") ? email = request.body["email"] : response.send("303", "invalid format");
+            request.body.hasOwnProperty("password") ? password = request.body["password"] : response.send("303", "invalid format");
+
+            admin.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                response.send ("303", "Failed user");
+            });
+
+            admin.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                response.send ("303", "Failed user");
+            });
+        }
 );
--- app/functions/myfuncts/readUserXProcessInstance.js
+++ app/functions/myfuncts/readUserXProcessInstance.js
@@ -1,5 +1,3 @@
-require ("./firebaseInit.js");
-
 function readUserXProcessInstance(processinstanceID){
-//TODO
+    //TODO
 };
